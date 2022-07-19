58aa837f3518a3839a5a3fd300909e8d782380dd
Ogaga <iyaraogaga69@gmail.com>
2022-07-19 09:57:37 +0100

    Modify Person class

--- person.rb
+++ person.rb
@@ -1,15 +1,15 @@
 # rubocop:disable Naming/PredicateName
 
 class Person
+    attr_reader :id
+    attr_accessor :name, :age, :parent_permission
+
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_reader :id
-  attr_accessor :name, :age, :parent_permission
 
   def can_use_services?
     is_of_age? || @parent_permission
